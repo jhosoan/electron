4f998553898c9eade2237f8e64f2a8bd8ea786f3
jhosoan <jhosoancordeiro17@gmail.com>
2025-04-11 10:03:55 -0300

    pasta criada

---
+++ main.js
@@ -0,0 +1,16 @@
+console.log("ola mundo!")
+
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  win.loadFile('./src/views/index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+})
